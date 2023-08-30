527ec9c65b667bdb5c78d62ea0e9a5b9dd8af055
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-30 11:34:27 +0600

    Exploring set!

--- py-set_intro.py
+++ py-set_intro.py
@@ -1,3 +1,11 @@
 '''
-Set in python:
+Set in python: Set is a unorderd collective data type that can hold any type of unique
+value in curley braces.
+in python set:
+set are mutable and iterable.
+set can accept any kind of immutable iterable data.
+set can't accept mutable iterable as argument.
+we can create a set by 2 ways:
+1. namesSet=set(iterable)
+2. namesSet={'msa','pvs','mhd','thd'}
 '''
